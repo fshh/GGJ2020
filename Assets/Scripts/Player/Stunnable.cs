203625f2cb5adaf2aab5d8dda5cfc662aaef1d91
rmcsheffdog <rmcsheff0913@gmail.com>
2020-02-01 20:02:55 -0500

    fiex cog stun in dock

--- Assets/Scripts/Player/Stunnable.cs
+++ Assets/Scripts/Player/Stunnable.cs
@@ -16,6 +16,7 @@ public class Stunnable : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        stunParticles.Stop();
         input = GetComponent<PlayerInput>();
         audioSource = GetComponent<AudioSource>();
         anim = GetComponent<Animator>();

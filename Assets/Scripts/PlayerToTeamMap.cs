4181c95b6536d56744271dac8b47cb23cc94090d
fshh <ajknollmeyer@gmail.com>
2020-02-02 09:01:58 -0500

    Set up controller navigation for UI on main menu, started team mapping logic

---
+++ Assets/Scripts/PlayerToTeamMap.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerToTeamMap
+{
+    // Map player numbers to their team
+    // 1 = team 1, 2 = team 2
+    private static Dictionary<PlayerInput.PlayerNumber, int> map = new Dictionary<PlayerInput.PlayerNumber, int>(4);
+
+    private static bool IsTeamFull(int team) {
+        int count = 0;
+        foreach (int t in map.Values) {
+            if (t == team) {
+                count++;
+            }
+        }
+        return count >= 2;
+    }
+
+    public static bool AssignPlayerToTeam(PlayerInput.PlayerNumber player, int team) {
+        if (team != 1 && team != 2) { return false; }
+
+        // If team is not full, assign to team
+        if (!IsTeamFull(team)) {
+            map[player] = team;
+            return true;
+        }
+
+        return false;
+    }
+
+    public static int GetPlayerTeam(PlayerInput.PlayerNumber player) {
+        return map[player];
+    }
+}
